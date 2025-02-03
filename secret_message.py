b7e7132924deb435ab77358e96d389b4e07aaa08
SamAtRectory <samantha.fafard@rectorywolves.org>
2025-02-03 11:34:28 -0500

    Made interactable function for CSV File.

--- secret_message.py
+++ secret_message.py
@@ -1,1 +1,19 @@
-print("Attackatdawn")
+import pandas as pd
+
+data = pd.read_csv("customers.csv")
+cols = data.columns.tolist()[1:]
+
+def count_str(c, f):
+	it = data[cols[c]].tolist()
+	
+	count = 0
+	for i in it:
+		if f in i:
+			count += 1
+	return count
+
+c_inp = input("What column are you looking at? \n" + ", ".join(cols) + "\n> ")
+c = cols.index(c_inp)
+f = input("What text are you looking for?\n> ")
+
+print(f"We found '{f}' {count_str(c+1, f)} times.")
