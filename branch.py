dea22c1892b80200fd9f851bacabf53ea2226c28
mkhurziya11 <khurziya.maratova@rectorywolves.org>
2025-02-03 06:55:28 -1000

    my function works now

--- branch.py
+++ branch.py
@@ -1,1 +1,25 @@
-print("Raining Tacos")
+import pandas as pd
+
+customers = pd.read_csv("customers.csv")
+
+
+for i in customers:
+
+	print(customers[i])
+
+inp1 = input("What column are you looking for? ")
+	
+inp2 = input("What item are you looking for? ")
+	
+	
+def dontknow(inp1, inp2):
+	count = 0
+		
+	column = customers[inp1]
+	
+	for i in column:
+		if inp2 == i or inp2 in i:
+			count += 1
+	print(count)
+
+dontknow(inp1, inp2)
