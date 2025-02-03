816ede8852a1313e821e221d9dfaa8fac56124de
Chris-C-at-Rectory <aokun.chen@rectorywolves.org>
2025-02-03 06:43:35 -1000

    Finished the function and have the results

--- something.py
+++ something.py
@@ -1,1 +1,21 @@
-print("This is my secret message")
+import pandas as pd
+
+def find(col,word):
+	app=0
+	for i in range(len(col)):
+		if col[i]==word:
+			app+=1
+		elif word in col[i]:
+			app+=1
+	return app
+	
+cus=pd.read_csv("customers.csv")
+cols=cus.columns.tolist()
+country=cus[cols[6]].tolist()
+company=cus[cols[4]].tolist()
+
+a=find(country, 'China')
+b=find(company, 'LLC')
+
+print('Customers from China',a)
+print('Customers from llc', b)
