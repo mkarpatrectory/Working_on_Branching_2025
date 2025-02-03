e8680e13e6de2ca8f5bff4853e2463a5b84fddf5
airconditionerale <ailee.choi@rectorywolves.org>
2025-02-03 11:51:36 -0500

    Finish coding function, information out put correctly

--- branching.py
+++ branching.py
@@ -1,10 +1,15 @@
+import pandas as pd
+
 def findc(ind,w):
 	org=pd.read_csv("customers.csv")
-	col=org.colums.tolist()
+	col=org.columns.tolist()
 	ai=org[col[ind]].tolist()
 	num=0
 	for i in ai:
 		if w in i: num+=1
-	return num
-
-
+	return str(num)
+	
+uin=int(input("Enter the index for the column:"))
+uw=input("Enter the word you need to find:")
+print("We find")
+print(uw+":"+findc(uin,uw))
