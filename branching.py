a84e1895b246634ab3bd5a86ce2909064489555e
airconditionerale <ailee.choi@rectorywolves.org>
2025-02-03 11:37:43 -0500

    Added the function thing

--- branching.py
+++ branching.py
@@ -1,1 +1,10 @@
-print("Hi Mrs. Karp! You're the best!")
+def findc(ind,w):
+	org=pd.read_csv("customers.csv")
+	col=org.colums.tolist()
+	ai=org[col[ind]].tolist()
+	num=0
+	for i in ai:
+		if w in i: num+=1
+	return num
+
+
