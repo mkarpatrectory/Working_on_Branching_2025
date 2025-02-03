d68ab5bd14e2c7431fe992d9fcebd42ce33a528b
LeoDengAtRectory <jingwen.deng@rectorywolves.org>
2025-02-03 11:55:56 -0500

    Almost finished User Inputs

--- Branch.py
+++ Branch.py
@@ -4,7 +4,7 @@ customers = pd.read_csv("customers.csv")
 cols=customers.columns.tolist()
 
 def cust(column,target):
-	col1 = customers[cols[column]].tolist()
+	col1 = customers[cols[cols.index(column)]].tolist()
 	
 	cols1 = 0
 	
@@ -13,12 +13,14 @@ def cust(column,target):
 			cols1 = cols1+1
 			
 	return cols1
-	
-cols1=cust(6,"China")
+
+y = input("What column are u looking for? (Customer Id, First Name, Last Name, Company, City, Country, Phone 1, Phone 2, Email, Subscribtion Date, Website)")
+x = input("what are u looking for?")	
+
+cols1=cust(y,x)
 print(cols1)
 
-cols2=cust(2, "John")
-print(cols2)
+
 
 
 """
